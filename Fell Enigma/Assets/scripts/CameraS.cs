6d69cd839a18f63e13eba35869e9cef8a3a5cac3
AzuraAiR <azuraair@live.com>
2017-06-24 00:10:59 +0800

    v0.111
    Manual uploading

---
+++ Fell Enigma/Assets/scripts/CameraS.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraS : MonoBehaviour {
+
+    public float Boundary = 50; // distance from edge scrolling starts
+    public float speed = 5;
+    public float min_x;
+    public float max_x;
+    public float min_y;
+    public float max_y;
+
+    private Vector2 screenDim;
+    private Vector3 newPos;
+
+	// Use this for initialization
+	void Start () {
+        screenDim = new Vector2(Screen.width, Screen.height);
+        min_x = transform.position.x - 5;
+        max_x = transform.position.x + 5;
+        min_y = transform.position.y - 5;
+        max_y = transform.position.y + 5;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        newPos = transform.position;
+
+        if (Input.mousePosition.x > screenDim.x - Boundary)
+        { 
+            newPos.x += speed * Time.deltaTime; // move on +X axis
+        }
+        if (Input.mousePosition.x < 0 + Boundary)
+        {
+            newPos.x -= speed * Time.deltaTime; // move on -X axis
+        }
+        if (Input.mousePosition.y > screenDim.y - Boundary)
+        {
+            newPos.y += speed * Time.deltaTime; // move on +Y axis
+        }
+        if (Input.mousePosition.y < 0 + Boundary)
+        {
+            newPos.y -= speed * Time.deltaTime; // move on -Y axis
+        }
+
+        transform.position = new Vector3(Mathf.Clamp(newPos.x, min_x, max_x), Mathf.Clamp(newPos.y, min_y, max_y), transform.position.z);
+    }
+}
---
+++ Fell Enigma/Assets/scripts/StatsUI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsUI: MonoBehaviour {
+
+    public Canvas statsWindow;
+    private Canvas UI;
+    private Slider healthBar;
+    private Text displayName;
+    private Text displayStats;
+    private Unit thisUnit;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (this.CompareTag("Player"))
+        {
+            thisUnit = this.GetComponent<PlayerUnit>();
+        }
+        else if (this.CompareTag("Enemy"))
+        {
+            thisUnit = this.GetComponent<AIUnit>();
+        }
+
+        UI = Instantiate(statsWindow, this.transform);
+        healthBar = UI.GetComponentInChildren<Slider>();
+        displayName = UI.GetComponentsInChildren<Text>()[0];
+        displayStats = UI.GetComponentsInChildren<Text>()[1];
+        loadStats();
+        UI.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    }
+
+    public void OnMouseOver()
+    {
+        updateData();
+        UI.enabled = true;
+    }
+
+    public void OnMouseExit()
+    {
+        UI.enabled = false;
+    }
+
+    private void updateData()
+    {
+        healthBar.value =  Mathf.Floor(((float) thisUnit.currentHP / (float) thisUnit.maxHP) * 100);
+    }
+
+    private void loadStats()
+    {
+        displayName.text = thisUnit.unitName;
+        displayStats.text = "ATK = " + thisUnit.strength.ToString() + " MAG = " + thisUnit.mag.ToString() + " SPD = " + thisUnit.spd.ToString() + "\n"
+            + "LUK = " + thisUnit.luk.ToString() + " DEF = " + thisUnit.def.ToString() + " RES = " + thisUnit.res.ToString();
+    }
+}
