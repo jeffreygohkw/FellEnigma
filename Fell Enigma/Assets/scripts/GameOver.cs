d2575de55092898cb4c4d9dccaefee1150adcd0f
jeffreygohkw <jeffreygohkw@users.noreply.github.com>
2017-06-25 15:16:20 +0800

    v0.14

---
+++ Fell Enigma/Assets/scripts/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour
+{
+	Animator anim;
+
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		if (Grid.instance.units[2][0].currentHP <= 0)
+		{
+			anim.SetTrigger("GameOver");
+		}
+		else if (Grid.instance.units[1][0].currentHP <= 0)
+		{
+			anim.SetTrigger("Victory");
+		}
+	}
+}
