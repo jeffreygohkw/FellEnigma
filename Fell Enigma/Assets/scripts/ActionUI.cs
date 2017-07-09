ff2b1c0ab8f517c362531ad7c20273c758f52572
AzuraAiR <azuraair@live.com>
2017-07-10 00:38:03 +0800

    v0.23b

---
+++ Fell Enigma/Assets/scripts/ActionUI.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ActionUI : MonoBehaviour
+{
+    private CanvasGroup thisCanvas;
+    private Text moveText;
+
+    private bool hasMoved;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        EventManager.StartListening("SelectUnit", SelectUnit);
+        EventManager.StartListening("MovedUnit", MovedUnit);
+        
+
+        thisCanvas = this.GetComponent<CanvasGroup>();
+        moveText = GetComponentsInChildren<Text>()[0];
+        OffUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SelectUnit()
+    {
+        OnUI();
+        EventManager.StopListening("SelectUnit", SelectUnit);
+        EventManager.StartListening("DeselectUnit", DeselectUnit);
+    }
+
+    void DeselectUnit()
+    {
+        OffUI();
+        EventManager.StartListening("SelectUnit", SelectUnit);
+        EventManager.StopListening("DeselectUnit", DeselectUnit);
+        hasMoved = false;
+        moveText.GetComponent<Text>().text = "Move";
+        this.GetComponentInChildren<Actions>().hasMoved = false;
+    }
+
+    void MovedUnit()
+    {
+        hasMoved = true;
+        moveText.GetComponent<Text>().text = "Undo Move";
+        this.GetComponentInChildren<Actions>().hasMoved = true;
+        EventManager.StartListening("UndoMovedUnit", UndoMovedUnit);
+        EventManager.StopListening("MovedUnit", MovedUnit);
+
+    }
+
+    void UndoMovedUnit()
+    {
+        hasMoved = false;
+        moveText.GetComponent<Text>().text = "Move";
+        this.GetComponentInChildren<Actions>().hasMoved = false;
+        EventManager.StopListening("UndoMovedUnit", UndoMovedUnit);
+        EventManager.StartListening("MovedUnit", MovedUnit);
+    }
+
+
+
+
+    private void OnUI()
+    {
+        thisCanvas.alpha = 1f;
+        thisCanvas.interactable = true;
+        thisCanvas.blocksRaycasts = true;
+    }
+
+    private void OffUI()
+    {
+        thisCanvas.alpha = 0f;
+        thisCanvas.interactable = false;
+        thisCanvas.blocksRaycasts = false;
+    }
+}
---
+++ Fell Enigma/Assets/scripts/Actions.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actions : MonoBehaviour {
+    public bool hasMoved = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void moveClick()
+    {
+        if (!hasMoved)
+        {
+            EventManager.TriggerEvent("MoveUnit");
+        }
+        else
+        {
+            EventManager.TriggerEvent("UndoMoveUnit");
+        }
+    }
+
+    public void attackClick()
+    {
+        EventManager.TriggerEvent("AttackUnit");
+    }
+
+    public void itemClick()
+    {
+        EventManager.TriggerEvent("ItemUnit");
+    }
+
+    public void waitClick()
+    {
+        EventManager.TriggerEvent("WaitUnit");
+    }
+
+    public void endClick()
+    {
+        EventManager.TriggerEvent("EndUnit");
+    }
+}
