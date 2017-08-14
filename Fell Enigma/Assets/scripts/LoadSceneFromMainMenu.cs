b38bb09d2b41c89dbdd4402fd46a1cc028c88b48
jeffreygohkw <jeffreygohkw@users.noreply.github.com>
2017-08-14 23:53:44 +0800

    v0.47e
    Button scripts

---
+++ Fell Enigma/Assets/scripts/LoadSceneFromMainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneFromMainMenu : MonoBehaviour {
+
+	public void LoadByName(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void Continue(string sceneName)
+	{
+		GameControl.instance.Load();
+		if (GameControl.instance.nextScene != "")
+		{
+			sceneName = GameControl.instance.nextScene;
+		}
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void exitGame()
+	{
+		UnityEditor.EditorApplication.isPlaying = false;
+		Application.Quit();
+	}
+}
